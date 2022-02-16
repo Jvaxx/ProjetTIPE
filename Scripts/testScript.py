43c99de9d23ba67e4206839a2b34b29b218c659d
Jvaxx <jeanvaleryze@gmail.com>
2022-02-16 21:21:28 +0100

    test ads 1

--- Scripts/testScript.py
+++ Scripts/testScript.py
@@ -6,7 +6,19 @@ from adafruit_ads1x15.analog_in import AnalogIn
 
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c, address=74)
+"""
+ADS 3 (73)  channel 0 ok, mal soudé
+            channel 1 ok
+            channel 2 ?
+            channel 3 ok
 
+ADS 2 (74)  channel 0 ok
+            channel 1 ok
+            channel 2 ?
+            channel 3 ok
+
+ADS 1 (  )  SDA mal soudé
+"""
 chan = AnalogIn(ads, ADS.P0, ADS.P1)
 while True:
     print(chan.value, chan.voltage)
