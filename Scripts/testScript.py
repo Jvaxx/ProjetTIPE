11fc394d170521e2d1c38567162539b001c1e534
Jean-Valery <jeanvaleryze@gmail.com>
2022-02-16 19:49:36 +0000

    commit

--- Scripts/testScript.py
+++ Scripts/testScript.py
@@ -1,3 +1,4 @@
+import time
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
@@ -6,6 +7,8 @@ from adafruit_ads1x15.analog_in import AnalogIn
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c, address=74)
 
-chan = AnalogIn(ads, ADS.P0)
-print(chan.value, chan.voltage)
+chan = AnalogIn(ads, ADS.P0, ADS.P1)
+while True:
+	print(chan.value, chan.voltage)
+	time.sleep(0.2)
 print('voila')
