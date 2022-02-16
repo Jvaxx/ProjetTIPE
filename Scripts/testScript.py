ed7ebe273a61e805d22b2a782bd240e2f753def3
Jvaxx <jeanvaleryze@gmail.com>
2022-02-16 20:03:29 +0100

    script

--- Scripts/testScript.py
+++ Scripts/testScript.py
@@ -1,4 +1,8 @@
 import board
 import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
 i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c, 1)
 print('voila')
